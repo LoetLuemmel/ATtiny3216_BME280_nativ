14a5d531fa8d81b1dc829097511c2f02fd60ac77
Pit Förster <p.foerster@sealog.de>
2024-11-17 11:23:19 +0100

    Switch to local BME280 library implementation

--- src/main.cpp
+++ src/main.cpp
@@ -8,30 +8,30 @@
 BME280 bme;
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
-  Serial.println(F("BME280 test"));
+    Serial.begin(9600);
+    while (!Serial);
+    Serial.println(F("BME280 test"));
 
-  Wire.begin(I2C_SDA, I2C_SCL);
-  Wire.setClock(100000);
-  delay(100);
+    Wire.begin(I2C_SDA, I2C_SCL);
+    Wire.setClock(100000);
+    delay(100);
 
-  bme.begin();
+    bme.begin();
 }
 
 void loop() {
-  Serial.print("Temperature = ");
-  Serial.print(bme.readTemperature());
-  Serial.println(" *C");
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" *C");
 
-  Serial.print("Pressure = ");
-  Serial.print(bme.readPressure() / 100.0); // Convert to hPa
-  Serial.println(" hPa");
+    Serial.print("Pressure = ");
+    Serial.print(bme.readPressure());
+    Serial.println(" hPa");
 
-  Serial.print("Humidity = ");
-  Serial.print(bme.readHumidity());
-  Serial.println(" %");
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
 
-  Serial.println();
-  delay(2000);
+    Serial.println();
+    delay(2000);
 }
