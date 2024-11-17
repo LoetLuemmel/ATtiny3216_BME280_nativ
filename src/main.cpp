8ca7a1f79e0f383d6ba6a2c8391c1985839b12bf
Pit Förster <p.foerster@sealog.de>
2024-11-17 10:40:32 +0100

    Add local BME280 library and update main code to use it

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,11 @@
-/***************************************************************************
-  This is a library for the BME680 gas, humidity, temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BME680 Breakout
-  ----> http://www.adafruit.com/products/3660
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
-
 #include <Arduino.h>
 #include <Wire.h>
-#include "BME280.h"  // Lokale Bibliothek statt Adafruit
+#include "BME280.h"
 
 #define I2C_SDA PIN_PB1
 #define I2C_SCL PIN_PB0
 
-BME280 bme;  // Verwendung der einfacheren BME280 Klasse
+BME280 bme;
 
 void setup() {
   Serial.begin(9600);
@@ -33,42 +16,20 @@ void setup() {
   Wire.setClock(100000);
   delay(100);
 
-  // Debug: I2C Scanner
-  Serial.println("Scanning I2C bus...");
-  byte error, address;
-  int nDevices = 0;
-  for(address = 1; address < 127; address++) {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    if (error == 0) {
-      Serial.print("I2C device found at address 0x");
-      if (address < 16) Serial.print("0");
-      Serial.println(address, HEX);
-      nDevices++;
-    }
-  }
-  if (nDevices == 0) {
-    Serial.println("No I2C devices found");
-  }
-
-  bme.begin();  // Einfachere Initialisierung
+  bme.begin();
 }
 
 void loop() {
-  float temp = bme.readTemperature();
-  float pressure = bme.readPressure();
-  float humidity = bme.readHumidity();
-
   Serial.print("Temperature = ");
-  Serial.print(temp);
+  Serial.print(bme.readTemperature());
   Serial.println(" *C");
 
   Serial.print("Pressure = ");
-  Serial.print(pressure);
+  Serial.print(bme.readPressure() / 100.0); // Convert to hPa
   Serial.println(" hPa");
 
   Serial.print("Humidity = ");
-  Serial.print(humidity);
+  Serial.print(bme.readHumidity());
   Serial.println(" %");
 
   Serial.println();
