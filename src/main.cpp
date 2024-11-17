84bde6509d2766c98a50255d192392c924ff8662
Pit Förster <p.foerster@sealog.de>
2024-11-17 10:34:03 +0100

    Switch from Adafruit BME680 to local BME280 library implementation

--- src/main.cpp
+++ src/main.cpp
@@ -17,19 +17,17 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include "Adafruit_BME680.h"
+#include "BME280.h"  // Lokale Bibliothek statt Adafruit
 
 #define I2C_SDA PIN_PB1
 #define I2C_SCL PIN_PB0
-#define SEALEVELPRESSURE_HPA (1028)
 
-Adafruit_BME680 bme(&Wire);
+BME280 bme;  // Verwendung der einfacheren BME280 Klasse
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
-  Serial.println(F("BME680 test"));
+  Serial.println(F("BME280 test"));
 
   Wire.begin(I2C_SDA, I2C_SCL);
   Wire.setClock(100000);
@@ -52,42 +50,26 @@ void setup() {
   if (nDevices == 0) {
     Serial.println("No I2C devices found");
   }
-  
-  if (!bme.begin(0x76)) {
-    Serial.println("Could not find BME680 sensor at 0x76!");
-    while (1);
-  }
-  Serial.println("BME680 sensor found!");
 
-  // Set up oversampling and filter initialization
-  bme.setTemperatureOversampling(BME680_OS_8X);
-  bme.setHumidityOversampling(BME680_OS_2X);
-  bme.setPressureOversampling(BME680_OS_4X);
-  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-  bme.setGasHeater(320, 150);
+  bme.begin();  // Einfachere Initialisierung
 }
 
 void loop() {
-  if (!bme.performReading()) {
-    Serial.println("Failed to perform reading :(");
-    return;
-  }
-  
+  float temp = bme.readTemperature();
+  float pressure = bme.readPressure();
+  float humidity = bme.readHumidity();
+
   Serial.print("Temperature = ");
-  Serial.print(bme.temperature);
+  Serial.print(temp);
   Serial.println(" *C");
 
   Serial.print("Pressure = ");
-  Serial.print(bme.pressure / 100.0);
+  Serial.print(pressure);
   Serial.println(" hPa");
 
   Serial.print("Humidity = ");
-  Serial.print(bme.humidity);
+  Serial.print(humidity);
   Serial.println(" %");
-
-  Serial.print("Gas = ");
-  Serial.print(bme.gas_resistance / 1000.0);
-  Serial.println(" KOhms");
 
   Serial.println();
   delay(2000);
